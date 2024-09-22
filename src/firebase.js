56b0a862272d6632ed464bcb0f57ca260f884a51
Abdul Hannan <86766068+abdulhanna@users.noreply.github.com>
2024-09-22 21:25:17 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getMessaging } from "firebase/messaging";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyBw8m-8LD8dKKKc1VwDGBzTFzXC4M6LUnQ",
-    authDomain: "goal-15435.firebaseapp.com",
-    projectId: "goal-15435",
-    storageBucket: "goal-15435.appspot.com",
-    messagingSenderId: "528693318655",
-    appId: "1:528693318655:web:9ef5e23727170a88aa0dca",
-    measurementId: "G-1QHC255KWM"
-  };
+// // Your web app's Firebase configuration
+   const firebaseConfig = {
+   apiKey: "YOUR_API_KEY",
+   authDomain: "YOUR_AUTH_DOMAIN",
+   projectId: "YOUR_PROJECT_ID",
+   storageBucket: "YOUR_STORAGE_BUCKET",
+   messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+   appId: "YOUR_APP_ID",
+   measurementId: "YOUR_MEASUREMENT_ID",
+ };
 
 export const app = initializeApp(firebaseConfig);
 export const messaging = getMessaging(app);
